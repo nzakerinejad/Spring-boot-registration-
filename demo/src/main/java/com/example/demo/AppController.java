bd7ec1bd09e4bc2479e063f1e3a8a5fc7a081d27
Negar <n.zakerinejad@gmail.com>
2024-06-05 22:00:46 +0200

    adding index page

---
+++ demo/src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String viewUser() {
+        return "hassan";
+    }
+
+}
